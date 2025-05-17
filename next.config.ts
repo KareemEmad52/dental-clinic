cb452a96925624cda9ddb5daeb0b591eca74ab31
Kareem Emad <98950173+KareemEmad52@users.noreply.github.com>
2025-05-17 17:48:27 +0300

    handling invoices

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+    ],
+  },
+  // experimental: {
+  //   esmExternals: 'loose',
+  // },
+  transpilePackages: ['@react-pdf/renderer'],
 };
 
 export default nextConfig;
